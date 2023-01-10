901fe3373a604cc4c678e79db0802ff0b0f075f0
Fumani Nyambi <fumaniglen@gmail.com>
2023-01-10 14:00:37 -0800

    Python data structures

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for i in matrix:
-        l = 1
-        for j in i:
-            if l == len(i):
-                print("{:d}".format(j), end="")
-            else:
-                print("{:d}".format(j), end=" ")
-            l = l + 1
+    for row in matrix:
+        count = 0
+        for elem in row:
+            count += 1
+            print('{:d}'.format(elem), end=(" " if count < len(row) else ""))
         print()
