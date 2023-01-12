e9f89229c25994a9d14901804a6b5f747520d81b
Fumani Nyambi <fumaniglen@gmail.com>
2023-01-11 18:44:37 -0800

    Python - More data structures:Set, dictionary

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
-def best_score(a_dictionary):
-    if not a_dictionary:
+def best_score(my_dict):
+    if my_dict is None or my_dict == {}:
         return None
-    biggest = 0
-    for key, value in a_dictionary.items():
-        if value > biggest:
-            biggest = value
-    for key, value in a_dictionary.items():
-        if value == biggest:
-            return 
+    biggest = max(my_dict.values())
+    for key, value in my_dict.items():
+        if value is biggest:
+            return key 
