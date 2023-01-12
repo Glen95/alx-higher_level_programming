90eb48b8340e2b153bb75c6e67a201b1c48c69ee
Fumani Nyambi <fumaniglen@gmail.com>
2023-01-11 18:48:33 -0800

    Python - More data structures:Set, dictionary

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    if not my_list:
-        return 0
-    res = 0.0
-    s_list = list(t[0] * t[1] for t in my_list)
-    w_list = list(t[1] for t in my_list)
-    res = sum(s_list) / sum(w_list)
-    return 
+    if my_list == [] or my_list is None:
+        return (0)
+    res = 0
+    res2 = 0
+    for x, y in my_list:
+        res += x * y
+        res2 += y
+    return (res / res2) 
