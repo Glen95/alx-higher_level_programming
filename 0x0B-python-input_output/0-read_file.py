bd681e9389cebca89d8ff2d2f11d7f6d3f47b4f4
Fumani Nyambi <fumaniglen@gmail.com>
2023-02-24 02:58:06 -0800

    Python - input_output

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
-read_file = __import__('0-read_file').read_file
+"""read_file
+"""
 
-read_file("my_file_0.txt")
+
+def read_file(filename=""):
+    """Takes in str filename to read it
+    """
+
+    with open(filename, encoding="utf-8") as readFile:
+        print(readFile.read(), end='')
