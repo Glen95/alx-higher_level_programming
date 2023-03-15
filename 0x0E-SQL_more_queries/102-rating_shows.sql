62d7f582cb4c2b046e271545e418d2f82b9e38e9
Fumani Nyambi <fumaniglen@gmail.com>
2023-03-15 05:14:48 -0700

    More Queries

---
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -0,0 +1,8 @@
+-- Lists all shows from hbtn_0d_tvshows_rate by their rating.
+-- Records are ordered by descending rating.
+SELECT tv_shows.title, SUM(ratings.rating) AS rating_sum
+FROM tv_shows
+JOIN ratings ON tv_shows.id = ratings.tv_show_id
+GROUP BY tv_shows.id
+ORDER BY rating_sum DESC;
+
